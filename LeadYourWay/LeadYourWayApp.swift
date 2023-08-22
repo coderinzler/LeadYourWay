2d7ed6ea24e25e49735be54bb03cf29ce1e7d1bb
kitagawaowo <uwu20201b980@gmail.com>
2023-08-22 12:57:37 -0500

    Initial Commit

---
+++ LeadYourWay/LeadYourWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeadYourWayApp.swift
+//  LeadYourWay
+//
+//  Created by Usuario de proyectos on 22/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LeadYourWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
